8c73da36d022ad6c95174c1f465ea227854fc905
merimano <meriem.manouchi@gmail.com>
2021-12-30 11:19:01 +0100

    Testing 3

--- pages/index.js
+++ pages/index.js
@@ -21,12 +21,12 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Meriem</p>
+        <p>Hi, I'm Meriem,</p>
         <p>
           I'm a fullstack developer and a marketing professional passionate about digital products. 
           Reach out at Meriem.Manouchi@gmail.com or 
           {' '}
-          <a href="https://www.linkedin.com/in/meriemmanouchi/"> find me at LinkedIn</a>!!
+          <a href="https://www.linkedin.com/in/meriemmanouchi/"> find me at LinkedIn</a>!
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
